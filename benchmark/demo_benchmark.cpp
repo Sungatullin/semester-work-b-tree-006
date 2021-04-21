d4fb98a7bc5e0dd5c8a9f91a0e1bb05e34b16570
4aspeak <79467160+Sungatullin@users.noreply.github.com>
2021-04-21 13:58:10 +0300

    Update demo_benchmark.cpp

--- benchmark/demo_benchmark.cpp
+++ benchmark/demo_benchmark.cpp
@@ -3,7 +3,6 @@
 #include <string>       // string, stoi
 #include <string_view>  // string_view
 #include <chrono>       // high_resolution_clock, duration_cast, nanoseconds
-#include <sstream>      // stringstream
 
 // подключаем вашу структуру данных
 #include "data_structure.hpp"
@@ -11,55 +10,101 @@
 using namespace std;
 using namespace itis;
 
+// Определите путь до проекта
+const string path_to_project = "C:\\Users\\4aspe\\Desktop\\C++\\semester-work-b-tree-006";
+
 // абсолютный путь до набора данных и папки проекта
-static constexpr auto kDatasetPath = string_view{PROJECT_DATASET_DIR};
-static constexpr auto kProjectPath = string_view{PROJECT_SOURCE_DIR};
+static const auto kDatasetPathAdd = path_to_project + "\\dataset\\data\\add\\";
+static const auto kDatasetPathSearch = path_to_project + "\\dataset\\data\\search\\";
+static const auto kDatasetPathRemove = path_to_project + "\\dataset\\data\\remove\\";
+static const int kSizeDataset = 100;
 
-int main(int argc, char **argv) {
-
-  // Tip 1: входные аргументы позволяют более гибко контролировать работу вашей программы.
-  // Можете передать путь до входного/выходного тестового файла в качестве аргумента программы.
-
-  for (int index = 0; index < argc; index++) {
-    cout << "Arg: " << argv[index] << '\n';
-  }
-
-  // Tip 2: для перевода строки в число можете использовать функцию stoi (string to integer)
-
-  // можете использовать функционал класса stringstream для обработки строки
-  auto ss = stringstream("0 1 2");  // передаете строку (входной аргумент или строку из файла) и обрабатываете ее
-
-  int number = 0;
-  ss >> number;  // number = 0
-  ss >> number;  // number = 1
-  ss >> number;  // number = 2
+int main() {
 
   // работа с набором данных
-  const auto path = string(kDatasetPath);
-  cout << "Path to the 'dataset/' folder: " << path << endl;
+  string path = kDatasetPathAdd;
 
-  auto input_file = ifstream(path + "/dataset-example.csv");
+  string path2 = kDatasetPathSearch;
 
-  if (input_file) {
-    // чтение и обработка набора данных ...
+  string path3 = kDatasetPathRemove;
+
+  for (int i = 1; i <= 10; i++) {
+    auto input_file = ifstream(path + "0" + to_string(i) + "\\" + to_string(kSizeDataset) + ".txt");
+    if (!input_file.is_open()) {
+      cout << "open " << path + "0" + to_string(i) + "\\" + to_string(kSizeDataset) + ".txt" << " error!" << endl;
+      return -1;
+    }
+
+    BTree b_tree(50);
+
+
+    const auto time_point_before = chrono::steady_clock::now();
+
+    while(!input_file.eof()){
+      int temp;
+      input_file >> temp;
+      b_tree.insert(temp);
+    }
+
+    const auto time_point_after = chrono::steady_clock::now();
+
+    // переводим время в наносекунды
+    const auto time_diff = time_point_after - time_point_before;
+    const long long time_elapsed_ns = chrono::duration_cast<chrono::nanoseconds>(time_diff).count();
+
+    cout << "Time elapsed to adding (ns): " << time_elapsed_ns << '\n';
+
+    //================================================================
+
+    auto input_file2 = ifstream(path2 + "0" + to_string(i) + "\\" + to_string(kSizeDataset) + ".txt");
+    if (!input_file2.is_open()) {
+      cout << "open " << i << " error!" << endl;
+      return -1;
+    }
+
+    const auto time_point_before2 = chrono::steady_clock::now();
+
+    while(!input_file2.eof()){
+      int temp;
+      input_file2 >> temp;
+      b_tree.search(temp);
+    }
+
+    const auto time_point_after2 = chrono::steady_clock::now();
+
+    // переводим время в наносекунды
+    const auto time_diff2 = time_point_after2 - time_point_before2;
+    const long long time_elapsed_ns2 = chrono::duration_cast<chrono::nanoseconds>(time_diff2).count();
+
+    cout << "Time elapsed to searching (ns): " << time_elapsed_ns2 << '\n';
+
+    //==================================================================
+
+    auto input_file3 = ifstream(path3 + "0" + to_string(i) + "\\" + to_string(kSizeDataset) + ".txt");
+    if (!input_file3.is_open()) {
+      cout << "open " << i << " error!" << endl;
+      return -1;
+    }
+
+    const auto time_point_before3 = chrono::steady_clock::now();
+
+    while(!input_file3.eof()){
+      int temp;
+      input_file3 >> temp;
+      b_tree.remove(temp);
+    }
+
+    const auto time_point_after3 = chrono::steady_clock::now();
+
+    // переводим время в наносекунды
+    const auto time_diff3 = time_point_after3 - time_point_before3;
+    const long long time_elapsed_ns3 = chrono::duration_cast<chrono::nanoseconds>(time_diff3).count();
+    cout << "Time elapsed to removing (ns): " << time_elapsed_ns3 << '\n';
+
+    // каждый проход выводится после нажатия любой клавиши
+    system("pause");
   }
 
-  // Контрольный тест: операции добавления, удаления, поиска и пр. над структурой данных
-
-  // Tip 3: время работы программы (или участка кода) можно осуществить
-  // как изнутри программы (std::chrono), так и сторонними утилитами
-
-  const auto time_point_before = chrono::steady_clock::now();
-
-  // здесь находится участок кода, время которого необходимо замерить
-
-  const auto time_point_after = chrono::steady_clock::now();
-
-  // переводим время в наносекунды
-  const auto time_diff = time_point_after - time_point_before;
-  const auto time_elapsed_ns = chrono::duration_cast<chrono::nanoseconds>(time_diff).count();
-
-  cout << "Time elapsed (ns): " << time_elapsed_ns << '\n';
-
   return 0;
+
 }
